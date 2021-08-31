4cbe8295f7bbd981d853e551da0bf85cc130f165
Michał Konopski <58469680+misiekhardcore@users.noreply.github.com>
2021-08-31 09:07:08 +0200

    Update LoginButton.js

--- src/routes/Login/components/LoginButton/LoginButton.js
+++ src/routes/Login/components/LoginButton/LoginButton.js
@@ -3,7 +3,7 @@ import "./_button.scss";
 import config from "../../../../config";
 
 const LoginButton = ({ children }) => {
-  const url = `https://accounts.spotify.com/authorize?client_id=${config.api.clientId}&response_type=code&redirect_uri=http://localhost:3000/login&scope=user-library-modify
+  const url = `https://accounts.spotify.com/authorize?client_id=${process.env.CLIENT_ID}&response_type=code&redirect_uri={process.env.REDIRECT_URL}&scope=user-library-modify
   user-library-read%20streaming%20user-read-private%20user-read-email%20user-read-playback-state%20user-modify-playback-state%20user-top-read`;
   return (
     <a className="button" href={url}>
