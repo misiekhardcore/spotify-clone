1fa78afc8cf109d33ec57bc5b5f0e223659b9c22
Michał Konopski <58469680+misiekhardcore@users.noreply.github.com>
2021-08-31 09:40:11 +0200

    Trying to fix env

--- src/routes/Login/components/LoginButton/LoginButton.js
+++ src/routes/Login/components/LoginButton/LoginButton.js
@@ -3,7 +3,7 @@ import "./_button.scss";
 //import config from "../../../../config";
 
 const LoginButton = ({ children }) => {
-  const url = `https://accounts.spotify.com/authorize?client_id=${process.env.CLIENT_ID}&response_type=code&redirect_uri=${process.env.REDIRECT_URL}&scope=user-library-modify
+  const url = `https://accounts.spotify.com/authorize?client_id=${process.env.REACT_APP_CLIENT_ID}&response_type=code&redirect_uri=${process.env.REACT_APP_REDIRECT_URL}&scope=user-library-modify
   user-library-read%20streaming%20user-read-private%20user-read-email%20user-read-playback-state%20user-modify-playback-state%20user-top-read`;
   return (
     <a className="button" href={url}>
