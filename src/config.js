f4e2e567de8da541b9f48f5d4bf25ef05c2dcd3b
Michał Konopski <58469680+misiekhardcore@users.noreply.github.com>
2021-08-31 08:48:50 +0200

    Update config.js

--- src/config.js
+++ src/config.js
@@ -3,8 +3,8 @@ export default {
   api: {
     baseUrl: "https://api.spotify.com/v1",
     authUrl: "https://accounts.spotify.com/api/token",
-    clientId: "",
-    clientSecret: "",
+    clientId: process.env.CLIENT_ID || "",
+    clientSecret: process.env.CLIENT_SECRET || "",
     redirectURL: "http://localhost:3000/login",
   },
 };
